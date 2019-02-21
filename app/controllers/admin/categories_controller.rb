a1fcd3d50491a3500a2c0b4dbff38066ee36e470
Romel Tan <romel.tan777@gmail.com>
2019-02-21 16:30:16 +0000

    added validation on creation of category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,7 +11,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Product created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
