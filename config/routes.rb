dd65c62ed9e372240e27dc2e4b3e370ce5920165
Romel Tan <romel.tan777@gmail.com>
2019-02-21 17:01:20 +0000

    added log in button, routes are correct.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  resource :users, only: [:create, :show, :new]
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
