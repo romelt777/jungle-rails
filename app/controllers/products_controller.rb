3a4928e2b3b879836e32f2f2c40a0fa765b12179
Romel Tan <romel.tan777@gmail.com>
2019-02-22 22:19:22 +0000

    able to display average rating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Rating.where({product_id: params[:id]})
+    @total_rating = 0;
   end
 
 end
