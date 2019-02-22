bf9e223f68ffd1394df144f8a9e2a10c76930b3b
Romel Tan <romel.tan777@gmail.com>
2019-02-22 15:28:43 +0000

    completed authentication of admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  before_filter :authorize
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
   end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  before_filter :authorize
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
