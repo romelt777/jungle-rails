9901b4bb44d3a21314f4c53fbd985731614b5710
Romel Tan <romel.tan777@gmail.com>
2019-02-23 03:25:22 +0000

    delete  button added for right user. able to click , redirect to root

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Rating.where({product_id: params[:id]}).reverse
-    @total_rating = 0;
+    @total_rating = 0
+    @current_user = current_user
+    puts "SCANDAL, #{@current_user.inspect}"
   end
 
 end
