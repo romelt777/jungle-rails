7c0868f33d9caaa7987429b142f26654fdb21b89
Romel Tan <romel.tan777@gmail.com>
2019-02-26 18:33:39 +0000

    ignores spaces in email.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email].downcase)
+    user = User.find_by_email(params[:email].downcase.tr(' ', ''))
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
